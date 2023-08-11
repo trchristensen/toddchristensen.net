099776d0947f21575f538fe91d3e4573438f87a5
Anon <github@christensen.wtf>
2023-08-11 18:39:54 +0800

    make excerpt fillable for post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -18,6 +18,7 @@ class Post extends Model implements HasMedia
     protected $fillable = [
         'title',
         'slug',
+        'excerpt',
         'body',
         'category_id',
         'user_id',
